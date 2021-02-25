8e2770b767833a662bb4a3902cfb63312abd4caa
Turtleburckle <59613582+Turtleburckle@users.noreply.github.com>
2021-02-25 11:24:58 +0200

    Bell - Laboratory

---
+++ Bell/cannonShot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannonShot : MonoBehaviour
+{
+    private bool canShot = false;
+    public AudioSource audioData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            canShot = true;
+            audioData.Play(0);
+        }
+        if (canShot) { transform.position += new Vector3(Time.deltaTime * -25, 0, 0); }
+    }
+}
---
+++ Bell/hittingWalls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hittingWalls : MonoBehaviour
+{
+    public AudioSource bell;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bell = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collider)
+    {
+        Debug.Log(collider.gameObject.name);
+        if (collider.gameObject.name == "Lareral1")
+        {
+            bell.Play();
+        }
+        if (collider.gameObject.name == "Lateral2") { bell.Play(); }
+        if (collider.gameObject.name == "Lateral3") { bell.Play(); }
+        if (collider.gameObject.name == "Lateral4") { bell.Play(); }
+    }
+
+}
